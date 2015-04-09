80bce20bf52353a69746a3b583e9f19026f657f0
Tomo Mizoe <mz@july.co.jp>
2015-04-09 19:22:03 +0900

    first version

---
+++ q25/q25/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 40)
+            {
+                Console.Write(i);
+                bool hasThree = false;
+                bool hasEight = false;
+                if (i % 3 == 0 || i.ToString().Contains("3"))
+                {
+                    hasThree = true;
+                }
+                if (i % 8 == 0 || i.ToString().Contains("8"))
+                {
+                    hasEight = true;
+                }
+                if (hasThree == true)
+                {
+                    if (hasEight == true)
+                    {
+                        Console.Write(" 無駄無駄無駄無駄");
+                    }
+                    else
+                    {
+                        Console.Write(" む!");
+                    }
+                }
+                else if (hasEight == true)
+                {
+                    Console.Write(" だ!");
+                }
+                Console.WriteLine();
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
