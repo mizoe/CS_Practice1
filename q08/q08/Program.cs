ef2f6a51f7f4094c5f53b33f9b509dd754064336
Tomo Mizoe <mz@july.co.jp>
2015-04-09 18:18:10 +0900

    最初のバージョン

---
+++ q08/q08/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ages = { 18, 11, 30, 15, 55, 32, 10 };
+        }
+    }
+}
