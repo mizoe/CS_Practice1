332303c5b60e30d7824647a031fb3d1ac80dc1a6
Tomo Mizoe <mz@july.co.jp>
2015-04-09 19:34:59 +0900

    first version

---
+++ q26/q26/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10000; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static bool IsPrime(int n)
+        {
+            if (n < 2)
+            {
+                return false;
+            }
+            else if (n == 2)
+            {
+                return true;
+            }
+
+            if (n % 2 == 0)
+            {
+                return false;
+            }
+
+            for (int j = 3; j <= n / j; j += 2)
+            {
+                if (n % j == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
