ee3df859a8058498f8208e015f9663bfda8231a6
Tomo Mizoe <mz@july.co.jp>
2015-04-09 18:20:35 +0900

    最初のバージョン

---
+++ q09/q09/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ages = { 18, 11, 30, 15, 55, 32, 10 };
+            foreach (var age in ages)
+            {
+                Console.Write("{0,2}", age);
+                if (age >= 20)
+                {
+                    Console.Write(" 成人");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
