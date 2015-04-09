46a036b4cc6cc4bf41a09d737f409fe8f7a5a14d
Tomo Mizoe <mz@july.co.jp>
2015-04-09 19:01:32 +0900

    first version

---
+++ q17/q17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint score;
+            string input = "";
+            while (uint.TryParse(input, out score) == false)
+            {
+                Console.WriteLine("成績を入力してください:");
+                input = Console.ReadLine();
+            }
+
+            if (score >= 80)
+            {
+                Console.WriteLine("A判定");
+            }
+            else if (score >= 70)
+            {
+                Console.WriteLine("B判定");
+            }
+            else if (score >= 60)
+            {
+                Console.WriteLine("C判定");
+            }
+            else
+            {
+                Console.WriteLine("D判定");
+            }
+            Console.ReadLine();
+        }
+    }
+}
