feb3363320ba91a4fdeda025608d94e0a0c7b790
Tomo Mizoe <mz@july.co.jp>
2015-04-09 19:08:36 +0900

    first version

---
+++ q21/q21/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("0から100までの合計は"+sum);
+            Console.ReadLine();
+        }
+    }
+}
