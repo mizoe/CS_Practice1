c8ced6ad6212bc641f5b64a6e515c84ddca3bf94
Tomo Mizoe <mz@july.co.jp>
2015-04-09 18:59:28 +0900

    first version

---
+++ q16/q16/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint stock;
+            string input = "";
+            while (uint.TryParse(input, out stock) == false)
+            {
+                Console.WriteLine("在庫を入力してください:");
+                input = Console.ReadLine();
+            }
+
+            if (stock == 0)
+            {
+                Console.WriteLine("在庫がありません");
+            }
+            else if (stock <= 3)
+            {
+                Console.WriteLine("在庫がわずかです");
+            }
+            else
+            {
+                Console.WriteLine("在庫があります");
+            }
+            Console.ReadLine();
+        }
+    }
+}
