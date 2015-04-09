dae10524beae74ac0adb0ceec7f77721c5479f0f
Tomo Mizoe <mz@july.co.jp>
2015-04-09 18:17:04 +0900

    最初のバージョン

---
+++ q06/q06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] places = { "青森→東京　", "飯田橋→渋谷", "飯田橋→横浜" };
+            double[] distances = { 715.0, 6.9, 33.8 };
+            double oneMile = 1.609344;
+            for (int i = 0; i < places.Length; i++)
+            {
+                Console.WriteLine
+                    ("{0} {1,6}km({2,6}マイル)",
+                        places[i],
+                        distances[i].ToString("F1"),
+                       (distances[i] / oneMile).ToString("F2")
+                    );
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ q07/q07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rooms = { "101", "102", "103" };
+            int[] area = { 100, 75, 150 };
+            double oneTsubo = 3.3;
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                Console.WriteLine
+                    ("{0}号室 {1,3}平方メートル({2,5}坪)",
+                        rooms[i],
+                        area[i],
+                       (area[i] / oneTsubo).ToString("F2")
+                    );
+            }
+            Console.ReadLine();
+        }
+    }
+}
