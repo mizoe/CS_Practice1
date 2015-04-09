f77e5b3a6e57af98fc3c03520f09c707a7b6584f
Tomo Mizoe <mz@july.co.jp>
2015-04-09 19:04:43 +0900

    first version

---
+++ q19/q19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.Write("おはよう");
+            }
+            Console.ReadLine();
+        }
+    }
+}
