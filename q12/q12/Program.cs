be6f24456897989f2f853ad18fa48d572d4db578
Tomo Mizoe <mz@july.co.jp>
2015-04-09 18:39:35 +0900

    first version

---
+++ q12/q12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("年齢を入力してください:");
+            string input = Console.ReadLine();
+            uint age = uint.Parse(input);
+            if (age >= 16 && age <= 69)
+            {
+                Console.WriteLine("献血できます");
+            }
+            else
+            {
+                Console.WriteLine("献血できません");
+            }
+            Console.ReadLine();
+        }
+    }
+}
