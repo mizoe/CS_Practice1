f95f9fdb32f652e04e7ad10d3bcac72372c8979f
Tomo Mizoe <mz@july.co.jp>
2015-04-09 19:03:24 +0900

    first version

---
+++ q18/q18/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 5)
+            {
+                Console.WriteLine("こんにちは");
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
