38f7b4d49656822844091a10dcba629dd90d4006
Tomo Mizoe <mz@july.co.jp>
2015-04-09 19:06:56 +0900

    first version

---
+++ q20/q20/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.Write("かねだ");
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("あ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
