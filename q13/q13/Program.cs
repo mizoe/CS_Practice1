690ba960ef71daffaa4884a26588b355a5e3759b
Tomo Mizoe <mz@july.co.jp>
2015-04-09 18:43:59 +0900

    first version

---
+++ q13/q13/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("年齢を入力してください:");
+            string input = Console.ReadLine();
+            uint age = uint.Parse(input);
+
+            Console.WriteLine("身長を入力してください:");
+            input = Console.ReadLine();
+            uint height = uint.Parse(input);
+
+            if (age >= 6 && height >= 120)
+            {
+                Console.WriteLine("乗れます");
+            }
+            else
+            {
+                Console.WriteLine("乗れません");
+            }
+            Console.ReadLine();
+        }
+    }
+}
