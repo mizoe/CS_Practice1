a7f9a3f92d781d404a3fb39e0dce3d4d7a98a8dd
Tomo Mizoe <mz@july.co.jp>
2015-04-09 19:17:16 +0900

    first version

---
+++ q24/q24/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 15)
+            {
+                Console.Write(i);
+                if (i % 3 == 0)
+                {
+                    Console.Write(" む!");
+                }
+                Console.WriteLine();
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
