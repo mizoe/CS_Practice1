e7263a433138d009b37657f63aa638602474306b
Tomo Mizoe <mz@july.co.jp>
2015-04-09 19:40:59 +0900

    first version

---
+++ q27/q27/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j != 0)
+                    {
+                        Console.Write("+");
+                    }
+                    Console.Write(j);
+                }
+                sum += i;
+                Console.WriteLine("=" + sum);
+            }
+            Console.ReadLine();
+        }
+    }
+}
