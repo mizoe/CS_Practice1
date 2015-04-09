22647670623744169676740f8c4bb1c40eaf9d16
Tomo Mizoe <mz@july.co.jp>
2015-04-09 18:03:40 +0900

    最初のバージョン

---
+++ q05/q05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lastNames  = { "Suzuki", "Tanaka", "Kimura", "Shima" };
+            string[] firstNames = { "Taro", "Hanako", "Jiro", "Tetsuo" };
+            int i = 0;
+            foreach (var lastName in lastNames)
+            {
+                Console.WriteLine(lastName + " " + firstNames[i]);
+                i++;
+            }
+            Console.WriteLine("終了するにはENTERキーを押してください。");
+            Console.ReadLine();
+        }
+    }
+}
