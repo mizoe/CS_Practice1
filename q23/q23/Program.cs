7f7d50adbbbc484e7b467f18f9709a23276428d8
Tomo Mizoe <mz@july.co.jp>
2015-04-09 19:15:25 +0900

    first version

---
+++ q23/q23/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 20)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
