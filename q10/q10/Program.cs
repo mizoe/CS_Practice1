eaadf23146b9b3e166f9c019ec8fe6575ff2b0be
Tomo Mizoe <mz@july.co.jp>
2015-04-09 18:24:11 +0900

    最初のバージョン

---
+++ q10/q10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ages = { 18, 11, 30, 15, 55, 32, 10 };
+            foreach (var age in ages)
+            {
+                Console.Write("{0,2}", age);
+                if (age >=7 && age <=12)
+                {
+                    Console.Write(" 小学生");
+                }
+                else if (age >= 13 && age <= 15)
+                {
+                    Console.Write(" 中学生");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
