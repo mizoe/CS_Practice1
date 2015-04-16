ae18817ccd1b2ce283e22617d04237e07dd3f677
Tomo Mizoe <mz@july.co.jp>
2015-04-16 20:15:13 +0900

    q40

--- 0_all/q40/Program.cs
+++ 0_all/q40/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace q38
+namespace q40
 {
     class Program
     {
@@ -12,6 +12,8 @@ namespace q38
         {
             Random rand = new Random();
             string input = "";
+            var arrHistory = new List<int[]>();
+            var history    = new List<int>();
             while (true)
             {
                 //1以上6未満の乱数を整数で返す
@@ -19,9 +21,31 @@ namespace q38
                 for (int i = 0; i < 3; i++)
                 {
                     dices[i] = rand.Next(1, 7);
+                    history.Add(dices[i]);
                     Console.Write(dices[i] + " ");
                 }
+                arrHistory.Add(dices);
                 input = Console.ReadLine();
+                if (input == "h")
+                {
+                    Console.WriteLine("   --- HISTORY ---");
+                    Console.Write("   ");
+                    foreach (var h in history)
+                    {
+                        Console.Write(h + " ");
+                    }
+                    Console.WriteLine();
+                    //履歴の統計情報
+                    for (int i = 1; i <= 6; i++)
+                    {
+                        int freq = history.Where(c => c.Equals(i)).Count();
+                        double ratio = freq / (double)history.Count * 100;
+                        Console.WriteLine("   {0}: {1,4}回 確率{2,4}%",
+                            i, freq, ratio.ToString("F1"));
+                    }
+                    Console.WriteLine("   試行数:" + history.Count);
+                    Console.WriteLine("   平均値:" + history.Average());
+                }
             }
         }
     }
