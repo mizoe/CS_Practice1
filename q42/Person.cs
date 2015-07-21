1d5e6a99dcef35c5aa3acf965a18da9dbaec3383
mizoe <mz@july.co.jp>
2015-07-21 09:46:31 +0900

    first version

---
+++ q42/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q42
+{
+    class Person
+    {
+        //フィールド
+        public string lastName; //姓
+        public string firstName; //名
+        public string furiganaLastName; //姓ふりがな
+        public string furiganaFirstName; //名ふりがな
+        public DateTime birthday; //誕生日
+
+        //メソッド
+        public int getAge()
+        {
+            int age = 0;
+            DateTime now = new DateTime();//現在
+            //参考：http://dobon.net/vb/dotnet/system/getage.html
+            age = now.Year - birthday.Year;
+            //誕生日前だったら
+            if (now.Month < birthday.Month || 
+                (now.Month == birthday.Month && now.Day < birthday.Day))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
